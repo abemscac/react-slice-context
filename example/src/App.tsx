7232f061b1411c6c5e4e7ac4e1a7738bf55813e9
abemscac <abe.msc.ac@gmail.com>
2024-01-07 07:28:50 +0800

    chore: gh pages auto deploy

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,12 +4,14 @@ import { PizzaPrice } from './components/PizzaPrice'
 import { RenderCount } from './components/RenderCount'
 import { TodoDispatch } from './components/TodoDispatch'
 import { TodoTasks } from './components/TodoTasks'
+import { ViewOnGitHub } from './components/ViewOnGitHub'
 
 export const App = () => {
   return (
     <div className="app section">
       <RenderCount />
       <h1>React Slice Context Demo 🍕</h1>
+      <ViewOnGitHub />
       <p>
         This component won't re-render unless you add some state to it and make
         changes.
--- example/src/components/RenderCount.tsx
+++ example/src/components/RenderCount.tsx
@@ -5,7 +5,10 @@ export const RenderCount = () => {
   const firstRender = useRef(true)
 
   useEffect(() => {
-    if (firstRender.current) {
+    // This check ensures that renderCount starts from 1 when
+    // StrictMode is enabled. If StrictMode is disabled in main.tsx,
+    // you can remove the extra check for the correct result.
+    if (import.meta.env.DEV && firstRender.current) {
       firstRender.current = false
     } else {
       ++renderCount.current
