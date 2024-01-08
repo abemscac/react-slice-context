5068ed54468986cac1f4495b732ae3ea3684b449
abemscac <abe.msc.ac@gmail.com>
2024-01-08 10:53:57 +0800

    docs: README update for plugins

--- src/i-plugin.ts
+++ src/i-plugin.ts
@@ -1,11 +1,12 @@
 export type IPlugin<State extends object> = {
   /**
-   * Called when context state is initialized.
+   * Called when the context state is initialized.
+   * @param state Read-only state after initialization.
    */
   onStateInit?: (state: State) => void
   /**
    * Called whenever there's a change in the context state.
-   * @param state The state after changes were made.
+   * @param state Read-only state after changes were made.
    */
   onChange?: (state: State) => void
 }
